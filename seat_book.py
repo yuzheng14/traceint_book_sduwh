50139f511cd209de77c8c0675ce5e287d0541c1c
yuzheng14 <422450117@qq.com>
2021-10-31 22:17:19 +0800

    捡漏测试成功

--- seat_book.py
+++ seat_book.py
@@ -3,8 +3,6 @@ import json
 import time
 
 # seat_status=1为可预订
-
-
 def post(post_para, headers):
 
     url = 'https://wechat.v2.traceint.com/index.php/graphql/'
@@ -57,27 +55,7 @@ def book(cookie):
                     continue
             else:
                 print(f"{seat['name']}号座位无法预定")
-        
 
-def book_test():
-    with open('json/book/book_para.json','r') as f:
-        book_para=json.load(f)
-    with open('json/book/10_resp.json','r') as f:
-        resp=json.load(f)
-
-    if 'errors' not in resp:
-        seats=resp["data"]["userAuth"]["reserve"]["libs"][0]["lib_layout"]["seats"]
-        seats.sort(key=take_seat_name)
-        while True:
-            for seat in seats:
-                if(seat["seat_status"]==1):
-                    book_para["variables"]["seatKey"]=seat["key"]
-                    print(book_para)
-                    try:
-                        print(f"预定成功，座位为{seat['name']}号")
-                        return
-                    except:
-                        continue
 def take_seat_name(elem):
     name=elem['name']
     if name != "" and name is not None:
@@ -85,5 +63,5 @@ def take_seat_name(elem):
     return 5000
 
 if __name__ == '__main__':
-    # book('FROM_TYPE=weixin; v=5.5; Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjIxMDAxOTM2LCJzY2hJZCI6MTI2LCJleHBpcmVBdCI6MTYzNTU1MzUzMH0.jgq_S3qlBx44o3hqqa08DVX2i6J3V2alOXJXGUq61R0RQ3SGCJkT9c15Pl4uJ_xps4_WXEbkuW3QahMfNDvsmG-lwK-w1f9KNcV001QojJIQ6H1qfZg6wYZzhmHogSZwTK9nYbNoV6zUz-yviBf_qj4FpgfAHWWwqwNDSPaj_MlKOmsDaYzIGS9aUJKkoqKpnqh7lkAuvlW-Mkhy0_mgG-MbzqB7u07A6cUz_RhuXlW4lq_JR675lgLLEG73k_UWl7QE_ABoRFcCnEDGGsUPB6GDqNVorAXGHhC0rbpbvqIodQQDeKgj4S_TBaTMuTJza48yjMUvKiqREvKAzjDyrw; Hm_lvt_7ecd21a13263a714793f376c18038a87=1635515712,1635519596,1635546155,1635549931; Hm_lpvt_7ecd21a13263a714793f376c18038a87=1635549931; SERVERID=82967fec9605fac9a28c437e2a3ef1a4|1635549942|1635546149')
-    book_test()
+    book('FROM_TYPE=weixin; v=5.5; Hm_lvt_7ecd21a13263a714793f376c18038a87=1635546155,1635549931,1635550504,1635568036; wechatSESS_ID=a035e5e1fee15ca19b254278aedff1d20f3c9fc907607210; Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjIxMDAxOTM2LCJzY2hJZCI6MTI2LCJleHBpcmVBdCI6MTYzNTY5MjcwN30.Ez20IR5pBg0EAdZnqj-ERDSy5pxFdwnf5zUhDz4vprHbiYZj-8gfz2x7ZBqYAP0Q1AhdQc4PVpOx_PRkeFevgSqoaP7-TdZa_GWhl6D4nFZf0I_9YcaMuWdxxaNa4eGTMCV4j_MfNzaILDlJiCGg2xMYFVn3Gkpw8ODQJAg188Z4csOOIWtR4roKzrL_oJ3C0ZW2VIweyBTO_RUfn3A1A84U_IjsLwiHHkpo_nMX71xI5gT9kDiIjGtKOXbxHGjwXsNuCNtFXmvqRjuqsh79rOSVpMQZ6rNbN8BG7-08VP_x5rG4Zv5R3nWvSHJYFp3inWRCVhT9ZQPZaqpVok2p6w; SERVERID=e3fa93b0fb9e2e6d4f53273540d4e924|1635689107|1635689099')
+    # book_test()
---
+++ test.py
@@ -0,0 +1,34 @@
+from seat_book import book,post,take_seat_name
+import json
+
+def cookie_test(cookie):
+    with open('json/book/10_para.json', 'r') as f:
+        post_para = json.load(f)
+    with open('json/book/10_headers.json','r') as f:
+        headers=json.load(f)
+    headers['Cookie']=cookie
+    resp=post(post_para,headers).json()
+    print(resp)
+
+def book_test():
+    with open('json/book/book_para.json','r') as f:
+        book_para=json.load(f)
+    with open('json/book/10_resp.json','r') as f:
+        resp=json.load(f)
+
+    if 'errors' not in resp:
+        seats=resp["data"]["userAuth"]["reserve"]["libs"][0]["lib_layout"]["seats"]
+        seats.sort(key=take_seat_name)
+        while True:
+            for seat in seats:
+                if(seat["seat_status"]==1):
+                    book_para["variables"]["seatKey"]=seat["key"]
+                    print(book_para)
+                    try:
+                        print(f"预定成功，座位为{seat['name']}号")
+                        return
+                    except:
+                        continue
+
+# 测试失效cookie
+cookie_test('FROM_TYPE=weixin; v=5.5; Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjIxMDAxOTM2LCJzY2hJZCI6MTI2LCJleHBpcmVBdCI6MTYzNTU1MzUzMH0.jgq_S3qlBx44o3hqqa08DVX2i6J3V2alOXJXGUq61R0RQ3SGCJkT9c15Pl4uJ_xps4_WXEbkuW3QahMfNDvsmG-lwK-w1f9KNcV001QojJIQ6H1qfZg6wYZzhmHogSZwTK9nYbNoV6zUz-yviBf_qj4FpgfAHWWwqwNDSPaj_MlKOmsDaYzIGS9aUJKkoqKpnqh7lkAuvlW-Mkhy0_mgG-MbzqB7u07A6cUz_RhuXlW4lq_JR675lgLLEG73k_UWl7QE_ABoRFcCnEDGGsUPB6GDqNVorAXGHhC0rbpbvqIodQQDeKgj4S_TBaTMuTJza48yjMUvKiqREvKAzjDyrw; Hm_lvt_7ecd21a13263a714793f376c18038a87=1635515712,1635519596,1635546155,1635549931; Hm_lpvt_7ecd21a13263a714793f376c18038a87=1635549931; SERVERID=82967fec9605fac9a28c437e2a3ef1a4|1635549942|1635546149')
