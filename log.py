d85ff111bd34715c1c0b70e4d88dfea60775bbf7
yuzheng14 <422450117@qq.com>
2021-11-02 09:49:28 +0800

    修改log为打印到控制台

--- log.py
+++ log.py
@@ -1,5 +1,6 @@
 import time
 
 def log(message):
-    with open('log.out','a',encoding='utf-8') as f:
-        f.write(time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime())+'\t'+f'{message}'+'\n')
+    # with open('log.out','a',encoding='utf-8') as f:
+        # f.write(time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime())+'\t'+f'{message}'+'\n')
+    print((time.strftime("[%Y-%m-%d %H:%M:%S] ", time.localtime())+'\t'+f'{message}'+'\n'))
--- seat_reserve.py
+++ seat_reserve.py
@@ -8,6 +8,7 @@ def seat_reserve(cookie):
     with open('pre_10_para.json','r') as f:
         pre_para=json.load(f)
     pre_headers['Cookie']=cookie
+    
     resp=post(pre_para,pre_headers).json()
 
 if __name__=='__main__':
