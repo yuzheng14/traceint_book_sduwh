6027fd480fea175d0e59b9954649b3380f9e3951
yuzheng14 <422450117@qq.com>
2021-11-10 12:46:44 +0800

    验证码

--- captcha.py
+++ captcha.py
@@ -21,26 +21,42 @@ def ocr():
     print(text)
     print(time.time()-start)
 
+def get_captcha_image():
+    pass
+
 def get_captcha(cookie):
-    wait_time(12,30)
-    if not verify_cookie(cookie):
-        log('cookie无效，请更新后重试')
     with open('json/reserve/get_end_time_headers.json','r') as f:
         get_end_time_headers=json.load(f)
     with open('json/reserve/get_end_time_para.json','r') as f:
         get_end_time_para=json.load(f)
     get_end_time_headers['Cookie']=cookie
-    resp_end_time=post(get_end_time_para,get_end_time_headers)
-    log(resp_end_time)
-    end_time=resp_end_time.json()['data']['userAuth']['prereserve']['endTime']
 
     with open('json/reserve/captcha_headers.json','r') as f:
         captcha_headers=json.load(f)
     with open('json/reserve/captcha_para.json','r') as f:
         captcha_para=json.load(f)
     captcha_headers['Cookie']=cookie
-    resp=post(captcha_para,captcha_headers).json()
-    log(resp)
+
+    if not verify_cookie(cookie):
+        log('cookie无效，请更新后重试')
+        return
+    
+    log('开始等待预定时间')
+    wait_time(12,30)
+    
+    
+    resp_end_time=post(get_end_time_para,get_end_time_headers)
+    log(resp_end_time)
+    end_time=resp_end_time.json()['data']['userAuth']['prereserve']['endTime']
+
+    resp=post(captcha_para,captcha_headers)
+    try:
+        resp=resp.json()
+    except:
+        log('body无json')
+        log(resp.content)
+        return
+    
     number=0
     while(time.time()<end_time ):
         if 'errors' not in resp:
@@ -54,4 +70,4 @@ def get_captcha(cookie):
     log('时间截止')
 
 if __name__=='__main__':
-    get_captcha('')
+    get_captcha('FROM_TYPE=weixin; v=5.5; Hm_lvt_7ecd21a13263a714793f376c18038a87=1636172899,1636259364,1636432169,1636446493; wechatSESS_ID=345f9db77839a9840268254f82712e3fc3861943a041887f; Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjIxMDAxOTM2LCJzY2hJZCI6MTI2LCJleHBpcmVBdCI6MTYzNjUyMTM2Mn0.Ill8I5AovfC4wlvG8FQMdugw1cOoLimCxDyfBqJx7pR8TOSc1Q8As9vhs48j7ZmLLEaLUIIc6B7tvYZbQkC22n_DpkKUgU2igbVxQnEbp8-vgS5y01y2-YSyQEJajQPT1xoWCZEZW-WCLiGja5DQWNA3MDDad8OENJxWg4ib_5ZrZMKNzhYzXuRJScse9OtJeBxOtKCl1jhRZnCiKcOU9D8JOy8KOt5Coyu4YdE-eWhH0Z3eJJhipQYKwKqAUmr7r_gNl-MlcG-4SDR29fO7czV8jY0fdrrV0RLhQBsjOUIHxw1zZm_8MqRPC03z5NvMrAkMJwELBw0ALoYHfCvuMQ; SERVERID=b9fc7bd86d2eed91b23d7347e0ee995e|1636517761|1636517755')
--- seat_reserve.py
+++ seat_reserve.py
@@ -13,8 +13,33 @@ def seat_prereserve(cookie):
         prereserve_headers=json.load(f)
     prereserve_headers['Cookie']=cookie
     prereserve_para["variables"]["key"] = '19,75'
+
+    with open('json/reserve/pre_10_headers.json','r') as f:
+        pre_headers=json.load(f)
+    with open('json/reserve/pre_10_para.json','r') as f:
+        pre_para=json.load(f)
+    pre_headers['Cookie']=cookie
+
+    with open('json/reserve/verify_captcha_headers.json','r') as f:
+        verify_captcha_headers=json.load(f)
+    with open('json/reserve/verify_captcha_para.json','r') as f:
+        verify_captcha_para=json.load(f)
+    verify_captcha_headers['Cookie']=cookie
+
     log('开始等待预定时间')
     wait_time(12,30)
+    log('尝试发送验证码')
+    log(json.dumps(verify_captcha_para,indent=4))
+    resp_verify_captcha=post(verify_captcha_para,verify_captcha_headers).json()
+    if  resp_verify_captcha['data']['userAuth']['prereserve']['verifyCaptcha']:
+        log('直接发送验证码成功')
+    else:
+        log('直接发送验证码失败')
+        log(json.dumps(resp_verify_captcha,indent=4))
+        return
+    
+    return
+    
     log("开始预定12号")
     prereserve_resp = post(prereserve_para, prereserve_headers).json()
     try:
@@ -23,22 +48,15 @@ def seat_prereserve(cookie):
             return
     except:
         log("预定12号失败")
-        log(prereserve_resp)
+        log(json.dumps(prereserve_resp,indent=4))
     
     log("预定12号失败")
-    log(prereserve_resp)
+    log(json.dumps(prereserve_resp,indent=4))
 
-    with open('json/reserve/pre_10_headers.json','r') as f:
-        pre_headers=json.load(f)
-    with open('json/reserve/pre_10_para.json','r') as f:
-        pre_para=json.load(f)
-    pre_headers['Cookie']=cookie
-    
-    
     resp=post(pre_para,pre_headers).json()
     while 'errors' in resp:
         log('请求座位失败')
-        log(resp)
+        log(json.dumps(resp,indent=4))
         time.sleep(1)
     seats = resp["data"]["userAuth"]["prereserve"]["libLayout"]["seats"]
     seats.sort(key=take_seat_name)
@@ -53,14 +71,14 @@ def seat_prereserve(cookie):
                     return
                 else:
                     log(f"预定{seat['name']}号失败")
-                    log(prereserve_resp)
+                    log(json.dumps(prereserve_resp,indent=4))
             except:
                 log(f"预定{seat['name']}号失败")
-                log(prereserve_resp)
+                log(json.dumps(prereserve_resp,indent=4))
                 continue
         else:
             log(f"{seat['name']}号座位无法预定")
 
 
 if __name__=='__main__':
-    seat_prereserve('FROM_TYPE=weixin; v=5.5; Hm_lvt_7ecd21a13263a714793f376c18038a87=1635826405,1635862471,1635913594,1636000027; wechatSESS_ID=bdf45042cc7568920a9a8810ebafc4e2f4d582ed5759e7fd; Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjIxMDAxOTM2LCJzY2hJZCI6MTI2LCJleHBpcmVBdCI6MTYzNjA4OTY0MX0.U2V8UAqEZVSSTMrLdpTaM_8b7Ad3yIBeVuSSGQRYgBbua4gi7k19MVyow0rbuj3zob7g407H1FNLAQRuBhZsCtQ6pscPIHs-s0xGq181WgxsapHm_JFioEfTQF_8-Qoxggg_s56xFRF8DmMAfHMd2Ub-vK7fud0pnWRJW7ba1CoPUOJsM_tbU1G1z9iaigWwr88ooAjOMk70q92cKQkeqy67adkm3v-jhAOmZV5U_J-ga1rB-htYbpmcTWkd95UmOIryGwQCH9RMTDNkK2F1aZlrZzsaUXWJ1KNz-MrMXSkDRqhzrtLQgcOXUCkhQ5TKrEAZPYmYIHs7Gk98mcJoxw; SERVERID=82967fec9605fac9a28c437e2a3ef1a4|1636086041|1636086035')
+    seat_prereserve('FROM_TYPE=weixin; v=5.5; Hm_lvt_7ecd21a13263a714793f376c18038a87=1636172899,1636259364,1636432169,1636446493; wechatSESS_ID=345f9db77839a9840268254f82712e3fc3861943a041887f; Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjIxMDAxOTM2LCJzY2hJZCI6MTI2LCJleHBpcmVBdCI6MTYzNjUyMTM2Mn0.Ill8I5AovfC4wlvG8FQMdugw1cOoLimCxDyfBqJx7pR8TOSc1Q8As9vhs48j7ZmLLEaLUIIc6B7tvYZbQkC22n_DpkKUgU2igbVxQnEbp8-vgS5y01y2-YSyQEJajQPT1xoWCZEZW-WCLiGja5DQWNA3MDDad8OENJxWg4ib_5ZrZMKNzhYzXuRJScse9OtJeBxOtKCl1jhRZnCiKcOU9D8JOy8KOt5Coyu4YdE-eWhH0Z3eJJhipQYKwKqAUmr7r_gNl-MlcG-4SDR29fO7czV8jY0fdrrV0RLhQBsjOUIHxw1zZm_8MqRPC03z5NvMrAkMJwELBw0ALoYHfCvuMQ; SERVERID=b9fc7bd86d2eed91b23d7347e0ee995e|1636517761|1636517755')
