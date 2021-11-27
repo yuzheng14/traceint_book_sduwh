4f1b6fb3c740c6a98a4ab26e6818a074c4eceba9
yuzheng14 <422450117@qq.com>
2021-11-27 19:22:18 +0800

    修改验证码相关代码

--- seat_reserve.py
+++ seat_reserve.py
@@ -1,4 +1,4 @@
-from utils import post,verify_cookie,take_seat_name,wait_time,log,on_close,on_error,on_message
+from utils import post,verify_cookie,take_seat_name,wait_time,log,on_close,on_error,on_message,save_unrecognized_image,save_recognized_image
 import json
 import time
 import ddddocr
@@ -58,26 +58,37 @@ def seat_prereserve(cookie):
     try:
         resp_get_end_time=post(get_end_time_para,get_end_time_headers).json()
         if resp_get_end_time['data']['userAuth']['prereserve']['getStep'] ==0:
+
             log('尝试识别验证码')
             resp_captcha=post(captcha_para,captcha_headers).json()
             captcha_code=resp_captcha['data']['userAuth']['prereserve']['captcha']['code']
             captcha_website=resp_captcha['data']['userAuth']['prereserve']['captcha']['data']
-            captcha=ocr.classification(requests.get(captcha_website).content)
+            image_byte=requests.get(captcha_website).content
+
+            captcha=ocr.classification(image_byte)
             log(f'识别验证码为{captcha}')
+
             verify_captcha_para['variables']['captcha']=captcha
             verify_captcha_para['variables']['captchaCode']=captcha_code
             resp_verify_captcha=post(verify_captcha_para,verify_captcha_headers).json()
+
             while not resp_verify_captcha['data']['userAuth']['prereserve']['verifyCaptcha']:
-                log(json.dumps(resp_verify_captcha,indent=4,ensure_ascii=False))
-                log(f'{captcha_code}尝试失败，开始下一次尝试')
+
+                log(f'{captcha_code}尝试失败，保存验证码图片后开始下一次尝试')
+                save_unrecognized_image(image_byte,'_'.join((captcha_code,captcha_website.split('/')[-1])))
+
                 resp_captcha=post(captcha_para,captcha_headers).json()
                 captcha_code=resp_captcha['data']['userAuth']['prereserve']['captcha']['code']
                 captcha_website=resp_captcha['data']['userAuth']['prereserve']['captcha']['data']
-                captcha=ocr.classification(requests.get(captcha_website).content)
+
+                image_byte=requests.get(captcha_website).content
+                captcha=ocr.classification(image_byte)
+
                 log(f'识别验证码为{captcha}')
                 verify_captcha_para['variables']['captcha']=captcha
                 verify_captcha_para['variables']['captchaCode']=captcha_code
                 resp_verify_captcha=post(verify_captcha_para,verify_captcha_headers).json()
+
             log(f'验证码尝试成功，验证码为{captcha}')
             log(json.dumps(resp_verify_captcha,indent=4,ensure_ascii=False))
         else:
@@ -150,6 +161,9 @@ def seat_prereserve(cookie):
     if 'wss' in dir(): # vars() locals().keys()均可
         wss.close()
         log('create_connection连接关闭')
+    if 'image_byte' in dir():
+        log('开始写入验证码图片')
+        save_recognized_image(image_byte,'_'.join((captcha,captcha_code,captcha_website.split('/')[-1])))
 
 if __name__=='__main__':
     seat_prereserve('FROM_TYPE=weixin; v=5.5; Hm_lvt_7ecd21a13263a714793f376c18038a87=1636950371,1637209490,1637287316,1637418775; wechatSESS_ID=ad316af26f39204b06b9dbbace48d2cfdd1a025177ae5e38; Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjIxMDAxOTM2LCJzY2hJZCI6MTI2LCJleHBpcmVBdCI6MTYzNzQ3MTk4MH0.PXvW0JPBbzyuet895fJ8HQNBvRkmjQzzDrs44TEuK_ikmA33VWwzdNerplQZB4Fmy1Djs4Pcz8bm35yxELOH4w0bRch6XsibMzSZKJ9YQv-hyG0n9wF_b92T9e6dP7m-eCfjD_tPFHwYXhAHvOsbnUOeL2PLFdQXDYrpAmkUUdG7HjBgXNGKlg4TylWKdvh2U8Tg3UT4-6H06C1LnhQDu3Bz7YgfNWzRj8axgh4rG2LIMrWT71CRbgkNjrtIXnuwdVvuQEJFIMuovTSSMaA0dTRWjUonef5LeoYz39W480Uo8iahHM2Aw9Ay0vxXmkU4BM8TrBly-77XUsMguFTFMQ; SERVERID=b9fc7bd86d2eed91b23d7347e0ee995e|1637468380|1637468378; Hm_lpvt_7ecd21a13263a714793f376c18038a87=1637468381')
