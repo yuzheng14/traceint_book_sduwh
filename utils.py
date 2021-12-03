a1883e56af785dcb639fd49df16f5cba9c62bf0a
yuzheng14 <422450117@qq.com>
2021-12-03 20:52:59 +0800

    部分重构

--- utils.py
+++ utils.py
@@ -3,33 +3,12 @@ import requests
 import time
 from enum import Enum
 import traceback
+from utils.request import post,verify_cookie
 
-def verify_cookie(cookie):
-    '''验证cookie有效性
-    参数
-    -------------------------------
-    cookie:str
-        传入cookie
 
-    返回值
-    -----------------------
-    bool
-        true为有效
-    '''
-    with open('json/book/index_headers.json') as f:
-        headers = json.load(f)
-    with open('json/book/index_para.json') as f:
-        para = json.load(f)
-    headers['Cookie'] = cookie
-    resp = post(para, headers).json()
-    return 'errors' not in resp
 
 # TODO doc注释
-def post(post_para, headers):
 
-    url = 'https://wechat.v2.traceint.com/index.php/graphql/'
-    resp = requests.request("post", url, json=post_para, headers=headers)
-    return resp
 
 # TODO doc注释
 def wait_time(hour,minute):
---
+++ utils/request.py
@@ -0,0 +1,28 @@
+import requests
+import json
+
+def post(post_para, headers):
+
+    url = 'https://wechat.v2.traceint.com/index.php/graphql/'
+    resp = requests.request("post", url, json=post_para, headers=headers)
+    return resp
+
+def verify_cookie(cookie):
+    '''验证cookie有效性
+    参数
+    -------------------------------
+    cookie:str
+        传入cookie
+
+    返回值
+    -----------------------
+    bool
+        true为有效
+    '''
+    with open('json/book/index_headers.json') as f:
+        headers = json.load(f)
+    with open('json/book/index_para.json') as f:
+        para = json.load(f)
+    headers['Cookie'] = cookie
+    resp = post(para, headers).json()
+    return 'errors' not in resp
