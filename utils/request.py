4a3ef518bb7262d4de339a281c47b153aa4e9b81
yuzheng14 <422450117@qq.com>
2021-12-12 23:08:27 +0800

    modify exception in get_captcha_code_website()

--- utils/request.py
+++ utils/request.py
@@ -13,6 +13,7 @@ class Activity(Enum):
     getStep = {"operationName": "getStep", "query": "query getStep {\n userAuth {\n prereserve {\n getStep\n queeUrl\n successUrl\n endTime\n }\n }\n}"}
     prereserveLibLayout = {"operationName": "libLayout", "query": "query libLayout($libId: Int!) {\n userAuth {\n prereserve {\n libLayout(libId: $libId) {\n max_x\n max_y\n seats_booking\n seats_total\n seats_used\n seats {\n key\n name\n seat_status\n status\n type\n x\n y\n }\n }\n }\n }\n}", "variables": {"libId": None}}
     index = {"operationName": "index", "query": "query index($pos: String!, $param: [hash]) {\n userAuth {\n oftenseat {\n list {\n id\n info\n lib_id\n seat_key\n status\n }\n }\n message {\n new(from: \"system\") {\n has\n from_user\n title\n num\n }\n indexMsg {\n message_id\n title\n content\n isread\n isused\n from_user\n create_time\n }\n }\n reserve {\n reserve {\n token\n status\n user_id\n user_nick\n sch_name\n lib_id\n lib_name\n lib_floor\n seat_key\n seat_name\n date\n exp_date\n exp_date_str\n validate_date\n hold_date\n diff\n diff_str\n mark_source\n isRecordUser\n isChooseSeat\n isRecord\n mistakeNum\n openTime\n threshold\n daynum\n mistakeNum\n closeTime\n timerange\n forbidQrValid\n renewTimeNext\n forbidRenewTime\n forbidWechatCancle\n }\n getSToken\n }\n currentUser {\n user_id\n user_nick\n user_mobile\n user_sex\n user_sch_id\n user_sch\n user_last_login\n user_avatar(size: MIDDLE)\n user_adate\n user_student_no\n user_student_name\n area_name\n user_deny {\n deny_deadline\n }\n sch {\n sch_id\n sch_name\n activityUrl\n isShowCommon\n isBusy\n }\n }\n }\n ad(pos: $pos, param: $param) {\n name\n pic\n url\n }\n}", "variables": {"pos": "App-首页"}}
+    verify_captcha = {"operationName": "setStep1", "query": "mutation setStep1($captcha: String!, $captchaCode: String!) {\n userAuth {\n prereserve {\n verifyCaptcha(captcha: $captcha, code: $captchaCode)\n setStep1\n }\n }\n}", "variables": {"captcha": "wvsb", "captchaCode": "ba315bdcb30fd7f55cd8fe1d443d4024"}}
 
 
 def post(post_para: dict, headers: dict) -> requests.Response:
@@ -185,16 +186,16 @@ def get_captcha_code_website(cookie: str) -> tuple:
 
     try:
         resp = resp.json()
-    except Exception as e:
-        log_info("当前响应无json")
-        log_info(resp.content)
-        raise e
-    try:
         result = ((resp['data']['userAuth']['prereserve']['captcha']['code'], resp['data']['userAuth']['prereserve']['captcha']['data']))
     except TypeError as e:
         log_info("json中无code及网址")
         log_info(_json=resp)
         raise e
+    except Exception as e:
+        log_info("当前响应无json")
+        log_info(resp.content)
+        raise e
+
     return result
 
 
@@ -214,6 +215,10 @@ def get_captcha_image(website: str) -> bytes:
     else:
         log_info('图片地址404')
         raise Exception("404 Not Found")
+
+
+def verify_captcha(cookie: str, captcha: str, captcha_code: str) -> bool:
+    para, headers = get_para_and_headers(Activity.verify_captcha, cookie)
 
 
 # TODO doc注释
