785af9745693c10e3796054f0fa3b4a9894999a4
yuzheng14 <422450117@qq.com>
2021-12-19 19:20:21 +0800

    restructure

--- seat_reserve.py
+++ seat_reserve.py
@@ -1,24 +1,21 @@
-import time
 import traceback
 
-import ddddocr
 import requests
 import websocket
 
 import json
-from utils.request import post, verify_cookie, get_ws_url, get_captcha_code_website, get_captcha_image, \
-    verify_captcha, get_queue_url, get_prereserve_libLayout, queue_init
-from utils.utils import log, save_recognized_image, save_unrecognized_image, take_seat_name, wait_time, log_info, \
-    seat_exist, pass_captcha
+from utils.request import post, verify_cookie, get_prereserve_libLayout, queue_init
+from utils.utils import log, take_seat_name, wait_time, log_info, \
+    seat_exist, pass_captcha, pass_queue
 
 
 # status=false时可以预定
-def seat_prereserve(cookie):
+def seat_prereserve(cookie: str, floor=10):
     if not verify_cookie(cookie):
         log('cookie无效，请重新输入cookie')
         return
 
-    ocr = ddddocr.DdddOcr()
+    lib_id = 758
 
     need_captcha, need_queue, ws_url, queue_url = queue_init(cookie)
 
@@ -43,43 +40,32 @@ def seat_prereserve(cookie):
     wait_time(12, 30)
 
     # 开始抢座
-    # TODO 一个函数获取need_captcha,need_queue,ws_url
     # 如果没有验证验证码，则开始验证验证码
     if need_captcha:
         log('当前未验证验证码，开始验证验证码')
-        ws_url=pass_captcha(cookie)
+        ws_url = pass_captcha(cookie)
     else:
         log('已验证验证码')
 
     try:
-        log(f'wss连接地址{ws_url}')
-        wss = websocket.create_connection(ws_url, timeout=30)
-        log('create_connection连接成功')
+        if need_captcha or need_queue:
+            log('当前需要排队，即将开始排队')
+            log_info(f'连接地址{ws_url}')
+            ws = websocket.create_connection(ws_url, timeout=30)
+            log_info('create_connection连接成功')
     except Exception:
+        log_info(f'\n{traceback.format_exc()}')
+        log_info('create_connection连接异常')
 
-        log('create_connection连接异常')
+    pass_queue(queue_url)
 
-
-    resp_queue = requests.get(queue_url)
-    queue_num = int(resp_queue.content)
-    log(f'前方排队{queue_num}人')
-    while queue_num > 0:
-        log(f'前方排队{queue_num}人')
-        if queue_num > 100:
-            time.sleep(2)
-        resp_queue = requests.get(queue_url)
-        queue_num = int(resp_queue.content)
-    log(f'前方排队{queue_num}人')
     log('排队完成')
 
     # 获取10楼的座位信息
-    seats = get_prereserve_libLayout(cookie, 758)
+    seats = [seat for seat in get_prereserve_libLayout(cookie, lib_id) if seat_exist(lib_id)]
     seats.sort(key=take_seat_name)
 
     for seat in seats:
-        if not seat_exist(seat):
-            log("该座位不存在")
-            continue
         if not seat["status"]:
             prereserve_para["variables"]["key"] = seat["key"]
             log(f"开始预定{seat['name']}号")
@@ -103,8 +89,8 @@ def seat_prereserve(cookie):
     queue_num = int(resp_queue.content)
     log(f'抢座完成后排队人数{queue_num}')
 
-    if 'wss' in dir():  # vars() locals().keys()均可
-        wss.close()
+    if 'ws' in dir():  # vars() locals().keys()均可
+        ws.close()
         log('create_connection连接关闭')
 
     resp_queue = requests.get(queue_url)
@@ -113,7 +99,6 @@ def seat_prereserve(cookie):
 
     if 'image_byte' in dir():
         log('开始写入验证码图片')
-        save_recognized_image(image_byte, captcha, captcha_code, captcha_website)
 
 
 if __name__ == '__main__':
