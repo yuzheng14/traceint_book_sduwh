c99483a057a19ba2d199853f278919922776c3c8
yuzheng14 <422450117@qq.com>
2021-12-21 22:26:14 +0800

    modify book()

--- seat_book.py
+++ seat_book.py
@@ -1,8 +1,8 @@
-import json
 import time
 
-from utils.utils import log, wait_time, take_seat_name
+import json
 from utils.request import post, verify_cookie
+from utils.utils import log, wait_time
 
 
 # seat_status=1为可预订
@@ -21,9 +21,9 @@ def book(cookie):
         log('cookie无效，请重新输入')
     log('开始等待开始时间')
     wait_time(7, 00)
-    log('等待九点钟')
-    wait_time(9, 00)
-    while (True):
+    # log('等待九点钟')
+    # wait_time(9, 00)
+    while True:
         resp = post(post_para, headers).json()
         if 'errors' in resp:
             log(resp)
@@ -32,11 +32,11 @@ def book(cookie):
 
         seats = resp["data"]["userAuth"]["reserve"]["libs"][0]["lib_layout"][
             "seats"]
-        seats.sort(key=take_seat_name)
+        seats.sort(key=lambda s: int(s['name']))
         for seat in seats:
             if seat['name'] == "" or seat['name'] is None:
                 continue
-            if (seat["seat_status"] == 1):
+            if seat["seat_status"] == 1:
                 book_para["variables"]["seatKey"] = seat["key"]
                 log(f"开始预定{seat['name']}号")
                 book_resp = post(book_para, book_headers).json()
@@ -51,7 +51,6 @@ def book(cookie):
 
 
 if __name__ == '__main__':
-    book(
-        'FROM_TYPE=weixin; v=5.5; Hm_lvt_7ecd21a13263a714793f376c18038a87=1638590314; wechatSESS_ID=acd56330154697ffc964552e73de519d6a5a94251ffe9378; Authorization=eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJ1c2VySWQiOjEwMDg2MjMsInNjaElkIjoxMjYsImV4cGlyZUF0IjoxNjM5MDk5MTM1fQ.cTQJe09lK_ecrH5pFm_MmADxX-bGu2Uw1dLBTvbT_GCKKwmk9henq9BGEjUSwzR2BQi82LSFnMIXnVT4lh0Zi4DOWdtY9lX9f0pdcYLhXv7iwERL_O1RAQhwFSilGgRryjF5f1R46h6IznyGUmZQ7cuJV67SZrxFDwe45wcePhHWVFdJ7rKgiug-QeZWVSNRdmKqkfbvrZZE_3eCbwrq2RmsR4KMQ8j7FXgda9ZHDaS8Zt7wC6UjzwFvpgkJ7yG6-SepH2l29qTJJvHvM9rWVW5O9Vj1-oVF5_dy5sO51XclmAQFILaceFHpHKt7Quxvy-J6kX4jscwIJcJug4Q1Rg; Hm_lpvt_7ecd21a13263a714793f376c18038a87=1639095818; SERVERID=d3936289adfff6c3874a2579058ac651|1639096044|1639095527'
-    )
+    cookie = ''
+    book(cookie)
     # book_test()
