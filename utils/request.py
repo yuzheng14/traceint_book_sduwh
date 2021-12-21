2712577111c1f2dbe6235616a3099e06746d472d
yuzheng14 <422450117@qq.com>
2021-12-21 23:02:35 +0800

    create function wait_for_reserve()

--- utils/request.py
+++ utils/request.py
@@ -480,13 +480,13 @@ def wait_for_start(cookie: str) -> Tuple[bool, bool, bool, str, str]:
     """
     if not verify_cookie(cookie):
         log_info('cookie无效，请重新输入cookie')
-        return False, None, None, None, None
+        return False, False, False, '', ''
     # 在开始明日预约前的1分钟确认cookie是否有效
     log('开始等待验证cookie时间')
     wait_time(12, 29)
     if not verify_cookie(cookie):
         log_info('cookie无效，请重新输入cookie')
-        return False, None, None, None, None
+        return False, False, False, '', ''
     else:
         log('cookie有效，请等待预定时间')
 
@@ -496,6 +496,32 @@ def wait_for_start(cookie: str) -> Tuple[bool, bool, bool, str, str]:
     need_captcha, need_queue, ws_url, queue_url = queue_init(cookie)
     return True, need_captcha, need_queue, ws_url, queue_url
 
+
+def wait_for_reserve(cookie: str) -> bool:
+    """
+    等待捡漏开始
+    Args:
+        cookie: headers中的cookie
+
+    Returns:
+        bool: true为cookie验证成功
+    """
+    if not verify_cookie(cookie):
+        log_info('cookie无效，请重新输入cookie')
+        return False
+    # 在开始明日预约前的1分钟确认cookie是否有效
+    log('开始等待验证cookie时间')
+    wait_time(6, 59)
+    if not verify_cookie(cookie):
+        log_info('cookie无效，请重新输入cookie')
+        return False
+    else:
+        log('cookie有效，请等待预定时间')
+
+    # 等待明日预约开始
+    log('开始等待预定时间')
+    wait_time(7, 00)
+    return True
 
 # TODO doc注释
 # TODO 完善函数
