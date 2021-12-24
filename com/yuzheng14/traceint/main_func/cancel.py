32f136008ef9534365439b00b6fb6cb95fba34af
yuzheng14 <422450117@qq.com>
2021-12-24 21:24:53 +0800

    restructure

--- com/yuzheng14/traceint/main_func/cancel.py
+++ com/yuzheng14/traceint/main_func/cancel.py
@@ -1,38 +1,18 @@
 import json
 
-from com.yuzheng14.traceint.utils.request import get_SToken, post, verify_cookie
+from com.yuzheng14.traceint.utils.request import get_SToken, post, verify_cookie, reserveCancle, pass_to_cancel
 from com.yuzheng14.traceint.utils.utils import log, wait_time, log_info
 
 
 # TODO restructure
 def cancel(cookie):
-    if not verify_cookie(cookie):
-        log('cookie失效，请输入有效cookie后重试')
-        return
+    """
+    退座
+    Args:
+        cookie: header中的cookie
+    """
+    if not pass_to_cancel(cookie):
+        log("cookie失效")
 
-    with open('../../../../json/cancel/cancel_para.json', 'r') as f:
-        cancel_para = json.load(f)
-    with open('../../../../json/cancel/cancel_header.json', 'r') as f:
-        cancel_header = json.load(f)
-    cancel_header['Cookie'] = cookie
-
-    log('开始等待验证cookie时间')
-    wait_time(22, 29)
-    if not verify_cookie(cookie):
-        log('cookie无效，请重新输入cookie')
-        return
-    else:
-        log('cookie有效，请等待预定时间')
-
-    log('等待固定时间')
-
-    wait_time(22, 30)
-
-    s_token = get_SToken(cookie)
-    cancel_para['variables']['sToken'] = s_token
-    cancel_resp = post(cancel_para, cancel_header)
-    log_info(_json=cancel_resp.json())
-
-
-if __name__ == '__main__':
-    cancel('')
+    while not reserveCancle(cookie):
+        pass
