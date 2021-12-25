98752785502b4987d4a40010a55a787707a0ad0c
yuzheng14 <422450117@qq.com>
2021-12-25 14:53:35 +0800

    add return for book and docstring

--- com/yuzheng14/traceint/traceint.py
+++ com/yuzheng14/traceint/traceint.py
@@ -3,8 +3,19 @@ from com.yuzheng14.traceint.main_func.seat_book import book
 from com.yuzheng14.traceint.main_func.seat_reserve import seat_prereserve
 
 
-def seat_pickup(cookie: str, often_floor: int = 3, strict_mode: bool = True, reserve: bool = False):
-    book(cookie, often_floor, strict_mode, reserve)
+def seat_pickup(cookie: str, often_floor: int = 3, strict_mode: bool = True, reserve: bool = False) -> bool:
+    """
+    座位闲时捡漏，函数将等待至7点开始运行
+    Args:
+        cookie: headers中的cookie
+        often_floor: 常用楼层
+        strict_mode: 是否为严格模式，默认为true，false则为遍历全部楼层
+        reserve: 是否倒序
+
+    Returns:
+        true为捡漏成功
+    """
+    return book(cookie, often_floor, strict_mode, reserve)
 
 
 def seat_reserve(cookie: str, floor: int = 10, often_seat: int = 1, reverse: bool = False):
