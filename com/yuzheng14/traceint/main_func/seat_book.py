48eeea439940b6b2c74ee681a0ddd9ac2d5cc505
yuzheng14 <422450117@qq.com>
2021-12-25 14:51:41 +0800

    add return for book

--- com/yuzheng14/traceint/main_func/seat_book.py
+++ com/yuzheng14/traceint/main_func/seat_book.py
@@ -1,11 +1,11 @@
 import time
 
-from com.yuzheng14.traceint.utils.utils import log
 from com.yuzheng14.traceint.utils.request import wait_for_reserve, have_seat, pass_reserve
+from com.yuzheng14.traceint.utils.utils import log
 
 
 # seat_status=1为可预订
-def book(cookie: str, often_floor: int = 3, strict_mode: bool = True, reserve: bool = False):
+def book(cookie: str, often_floor: int = 3, strict_mode: bool = True, reserve: bool = False) -> bool:
     """
     闲时捡漏
     Args:
@@ -13,16 +13,24 @@ def book(cookie: str, often_floor: int = 3, strict_mode: bool = True, reserve: b
         often_floor: 常用楼层
         strict_mode: 是否为严格模式，默认为true，false则为遍历全部楼层
         reserve: 是否倒序
+    Returns:
+        true为捡漏成功
     """
     # 验证cookie并等待开始抢座
     if not wait_for_reserve(cookie):
         log('cookie无效，请改正后重试')
-        return
+        return False
 
     # 循环遍历抢座
     while True and not have_seat(cookie):
-        pass_reserve(cookie, often_floor, strict_mode, reserve)
+        seat = pass_reserve(cookie, often_floor, strict_mode, reserve)
+        if seat != '':
+            log(f'预定成功，座位为{seat}号')
+            return True
         time.sleep(2)
+    if have_seat(cookie):
+        return True
+    return False
 
 
 if __name__ == '__main__':
