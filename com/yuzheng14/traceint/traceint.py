7eb698f31b5452dd7491fc3551a13ebe414ed59d
yuzheng14 <422450117@qq.com>
2021-12-25 14:59:02 +0800

    docstring seat_cancel()

--- com/yuzheng14/traceint/traceint.py
+++ com/yuzheng14/traceint/traceint.py
@@ -18,9 +18,17 @@ def seat_pickup(cookie: str, often_floor: int = 3, strict_mode: bool = True, res
     return book(cookie, often_floor, strict_mode, reserve)
 
 
-def seat_reserve(cookie: str, floor: int = 10, often_seat: int = 1, reverse: bool = False)->bool:
+def seat_reserve(cookie: str, floor: int = 10, often_seat: int = 1, reverse: bool = False) -> bool:
     return seat_prereserve(cookie, floor, often_seat, reverse)
 
 
-def seat_cancel(cookie: str)->bool:
+def seat_cancel(cookie: str) -> bool:
+    """
+    退座
+    Args:
+        cookie: headers中的cookie
+
+    Returns:
+        true为退座成功
+    """
     return cancel(cookie)
